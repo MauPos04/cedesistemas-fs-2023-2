74cc40efe62536292b9823939a2c97e14e1e1b3a
MauPos04 <mauriciop1palma@gmail.com>
2023-12-25 23:07:15 -0500

    Angular Register

--- Angular/lineabase/lineabase-angular/src/app/UI/components/register/register.component.ts
+++ Angular/lineabase/lineabase-angular/src/app/UI/components/register/register.component.ts
@@ -1,18 +1,17 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [ReactiveFormsModule, RouterModule, NgIf, NgFor],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
 })
-export class LoginComponent implements OnInit {
-
-  loginForm! : FormGroup;
+export class RegisterComponent implements OnInit {
+  registerform! : FormGroup;
   public validationMessages = {
     email:[
       {type: 'pattern', message: 'solo se permiten campos de tipo email'},
@@ -27,7 +26,7 @@ export class LoginComponent implements OnInit {
   }
   constructor(private router: Router, private formBuilder: FormBuilder){}
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
+    this.registerform = this.formBuilder.group({
       email:[
         '',
         [
@@ -50,14 +49,14 @@ export class LoginComponent implements OnInit {
   }
 
   public get getControls(){
-    return this.loginForm.controls
+    return this.registerform.controls
   }
-  login () {
-    var user = this.loginForm.controls['email'].value;
-    var password = this.loginForm.controls['password'].value;
-    if(this.loginForm.valid){
+  register () {
+    var user = this.registerform.controls['email'].value;
+    var password = this.registerform.controls['password'].value;
+    if(this.registerform.valid){
       if(user == 'admin@admin.com' && password == 'Colombia2023*'){
-        this.router.navigate(['/home'])
+        this.router.navigate(['/fullscreen/login'])
         return;
       }
       alert('usuario o contraseña invalido intente de nuevo')
--- Angular/lineabase/lineabase-angular/src/app/app.routes.ts
+++ Angular/lineabase/lineabase-angular/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './UI/components/home/home.component';
 import { ContactusComponent } from './UI/components/contactus/contactus.component';
 import { FullscreenComponent } from './UI/layouts/fullscreen/fullscreen.component';
 import { LoginComponent } from './UI/components/login/login.component';
+import { RegisterComponent } from './UI/components/register/register.component';
 
 export const routes: Routes = [
   {path:'', redirectTo:'/fullscreen/login', pathMatch:'full'},
@@ -29,6 +30,10 @@ export const routes: Routes = [
       {
         path:'login',
         component: LoginComponent
+      },
+      {
+        path:'register',
+        component: RegisterComponent
       }
     ]
   }
